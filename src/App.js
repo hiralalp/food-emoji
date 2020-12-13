6561908f6daee87c9f5577a7b129aa999aca3929
hiralalp <hiralalppurohit@gmail.com>
2020-12-13 10:34:25 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojiDictionary = {
+  "🍇": "Grapes",
+  "🍈": "Melon",
+  "🍉": "Watermelon",
+  "🍊": "Tangerine",
+  "🍋": "Lemon",
+  "🍌": "Banana",
+  "🍍": "Pineapple",
+  "🥭": "Mango",
+  "🍎": "Red Apple",
+  "🍏": "Green Apple",
+  "🍐": "Pear",
+  "🍑": "Peach",
+  "🍒": "Cherries",
+  "🍓": "Strawberry",
+  "🥝": "Kiwi Fruit",
+  "🍅": "Tomato",
+  "🥥": "Coconut",
+  "🥑": "Avocado",
+  "🍆": "Eggplant",
+  "🥔": "Potato",
+  "🥕": "Carrot",
+  "🌽": "Ear of Corn",
+  "🌶️": "Hot Pepper",
+  "🥒": "Cucumber",
+  "🥬": "Leafy Green",
+  "🥦": "Broccoli",
+  "🧄": "Garlic",
+  "🧅": "Onion",
+  "🍄": "Mushroom",
+  "🥜": "Peanuts",
+  "🌰": "Chestnut",
+  "🍞": "Bread",
+  "🥐": "Croissant",
+  "🥖": "Baguette Bread",
+  "🥨": "Pretzel",
+  "🥯": "Bagel",
+  "🥞": "Pancakes",
+  "🧇": "Waffle",
+  "🧀": "Cheese Wedge",
+  "🍖": "Meat on Bone",
+  "🍔": "Hamburger",
+  "🍟": "French Fries",
+  "🍕": "Pizza",
+  "🌭": "Hot Dog",
+  "🥪": "Sandwich",
+  "🌮": "Taco",
+  "🌯": "Burrito",
+  "🥗": "Green Salad",
+  "🍿": "Popcorn",
+  "🧈": "Butter",
+  "🧂": "Salt",
+  "🍚": "Cooked Rice",
+  "🍛": "Curry Rice",
+  "🍜": "Steaming Bowl",
+  "🍝": "Spaghetti",
+  "🍠": "Roasted Sweet Potato",
+  "🍣": "Sushi"
+};
+
+const emojis = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [emoji, setEmoji] = useState("");
+  const [meaning, setMeaning] = useState("translation will appear here..");
+
+  function changeHandler(event) {
+    const inputEmoji = event.target.value;
+    setEmoji(inputEmoji);
+
+    if (inputEmoji in emojiDictionary) {
+      setMeaning(emojiDictionary[inputEmoji]);
+    } else {
+      setMeaning("failure to recognise this emoji");
+    }
+  }
+
+  function emojiClickHandler(inputEmoji) {
+    setMeaning(emojiDictionary[inputEmoji]);
+  }
+
+  return (
+    <div className="App">
+      <h1>Inside Out</h1>
+      <input
+        onChange={changeHandler}
+        value={emoji}
+        placeholder={"Paste/type/Insert the emoji here"}
+        style={{
+          padding: "1em",
+          minWidth: "80%"
+        }}
+      />
+      <h2> {emoji} </h2> {}
+      <h3> {meaning} </h3> {}
+      {emojis.map((emoji) => (
+        <span
+          onClick={() => emojiClickHandler(emoji)}
+          style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+        >
+          {" "}
+          {emoji}{" "}
+        </span>
+      ))}
+    </div>
+  );
+}
